3d41af4d60ab5351097c832e2f3dc258282d2f4f
Eddy Cramm <eddy.cramm@gmail.com>
2026-02-23 21:39:03 -0800

    main.jsx update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.jsx' // Changed from ./golf_tournament.jsx
-<app></app>.jsx' // Note: app matches your filename
+import App from './app.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
