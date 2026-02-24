f84dae8544e617acd4a8a06f47b5c25477c27e1d
Eddy Cramm <eddy.cramm@gmail.com>
2026-02-23 21:32:21 -0800

    fix import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './golf_tournament.jsx' // Note: This matches your filename
+import App from './.jsx' // Note: app matches your filename
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
